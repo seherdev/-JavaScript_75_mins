d5225f4db4cfdaac90c5eed8f45563ba0f0f90d1
seherdev <7iletisimseherarslanbay@gmail.com>
2025-08-24 19:44:12 +0300

    4

--- script.js
+++ script.js
@@ -41,3 +41,42 @@ if(true) {
 } else if(false) {
     console.log("this is false")}
     
+
+//ARRAYS ____________________________________________
+/*
+const arr = [1, 2, 3, 4, 5];
+const arr2 = new Array(1, 2, 3, 4, 5);
+console.log(arr2[0]);
+
+// array destructuring (rest operator)
+const arr4 = [1, 2, 3, 4, 5, 6];
+const [x, x2, ...y] = arr4;
+console.log(x, x2, y); // 1 2 [3,4,5,6]
+
+// spread operator
+const arr5 = [...arr, 6, 7, 8];
+console.log(arr5); // [1,2,3,4,5,6,7,8]
+
+
+//>spread operator
+const arr3 = [...arr, 6, 7, 8] //new array  
+console.log()
+*/
+
+
+//LOOPS ____________________________________________
+const arr = [1, 2, 3];
+
+for (let /*u cannot use const*/ i = 0 ; i < 10; i++){
+    console.log(arr[i])
+    continue
+}
+
+
+//YA DA
+const arr2 = [4, 5, 6];
+
+for (let value of arr2){
+    console.log(value)
+    
+}
